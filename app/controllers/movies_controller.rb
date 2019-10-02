2029e4e338e7929a4bd51a2937b0efb6189e81db
arturoSG88 <arturosg88@gmail.com>
2019-10-01 19:50:50 -0500

    part 2 & 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,30 +1,56 @@
 class MoviesController < ApplicationController
 
   def show
-    id = params[:id] 
-    @movie = Movie.find(id) 
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
   def index
-    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    @checked_ratings = check
-    @checked_ratings.each do |rating|
-      params[rating] = true
+    @all_ratings = Movie.rates.keys
+
+    if not session[:ratings]
+      session[:ratings] = Movie.rates
+    end
+    if not session[:sort]
+      session[:sort] = "none"
     end
 
-    if params[:sort]
-      @movies = Movie.order(params[:sort])
-    else
-      @movies = Movie.where(:rating => @checked_ratings)
+    if(params[:sort])
+      session[:sort] = params[:sort]
+    end
+    if(params[:ratings])
+      session[:ratings] = params[:ratings]
+    end
+
+    if(not params[:sort] or not params[:ratings])
+      flash.keep
+      redirect_to movies_path :sort => session[:sort], :ratings => session[:ratings]
+    end
+
+    @movies = Movie.movie_rate(session[:ratings].keys)
+    @rating_checks = session[:ratings].keys
+
+    movies_order = session[:sort]
+    if(movies_order == "title")
+      @movies = @movies.order(:title)
+      @highlight_movie = "hilite"
+    elsif (movies_order == "date")
+      @movies = @movies.order(:release_date)
+      @highlight_date = "hilite"
     end
   end
 
   def new
-    
+    # default: render 'new' template
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -35,7 +61,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -45,16 +71,6 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
-  end
-
-  private
-
-  def check
-    if params[:ratings]
-      params[:ratings].keys
-    else
-      @all_ratings
-    end
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+  def self.rates
+    return {'G'=>1,'PG'=>1,'PG-13'=>1,'R'=>1}
+  end
+
+  def self.movie_rate(movie_ratings)
+    movies = Movie.where('rating in (?)',movie_ratings)
+    return movies
+  end
 end
